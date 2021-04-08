1eb82f2107bc660068369cecc423c9cf0526ced8
Dillon TenBrink <dillon@tenbrink.us>
2021-04-07 22:03:28 -0500

    Add Garmin Pilot logbook format parsing (#2)
    * Update ignore file for testing
    * Added experimental parsing for Garmin Pilot logs and changed project name.
    * Update project name
    * Adding empty from/to check back to code base
    * Update text for log type
    * Package version bump
    * Git URL update

--- main.js
+++ main.js
@@ -8,21 +8,25 @@ const fastcsv = require('fast-csv');
 
 // TODO: Fixed paths right now, to get the initial code out. Will make this dynamic later.
 const airportDataFile = "data/airports.csv";
-const logDataFile = "data/logbook.csv";
+const logDataFile = process.argv[2] || "data/logbook.csv";
 let airports = [];
+let logbookFormat = {};
 
 const go = async () => {
   airports = await getAirports();
   console.log(`Loaded airport directory, ${airports.length} airports found.`);
 
-  logData = await getLogbook();
-  console.log(`Loaded ForeFlight logbook, ${logData.length} flights found.`)
+  logFormat = await detectLogbookFormat();
+  console.log(`Detected logbook format of ${logFormat}.`);
+
+  logData = await getLogbook(logFormat);
+  console.log(`Loaded ${logFormat} logbook, ${logData.length} flights found.`)
 
   mapData = await exportToKepler(logData);
-  console.log("done");
+  console.log(`Completed export to kepler.gl format at out.csv.`)
 };
 
-const exportToKepler = async(logData) => {
+const exportToKepler = async (logData) => {
   // This does the work to reformat log data into the kepler.gl csv export.
   let kpd = [];
   for await (const el of logData) {
@@ -46,8 +50,8 @@ const exportToKepler = async(logData) => {
   fastcsv
     .write(kpd, { headers: true })
     .pipe(ws);
-  
-    return('done');
+
+  return ('done');
 };
 
 const getAirports = async () => {
@@ -55,7 +59,7 @@ const getAirports = async () => {
   return csv(stripBom(data));
 };
 
-const getLogbook = async () => {
+const getLogbook = async (logFormat) => {
   const fileStream = fs.createReadStream(logDataFile, 'utf-8');
   const rl = readline.createInterface({
     input: fileStream,
@@ -65,54 +69,88 @@ const getLogbook = async () => {
   let output = [];
 
   for await (const line of rl) {
+    // Log Parsing Regex Switcher
+    // TODO: Switch this to a csv parser, but will require editing the ForeFlight file to exclue the airplane info lines.
+    // For now, the Garmin hack is a fixed length CSV regex that can probably be optimized further.
+    // TODO: Also, this has a weird race condition if not evaluated each time. That needs to be fixed as well. 
+    let re;
+    if (logFormat == 'ForeFlight') {
+      re = /^(?<date>\d{4}-\d{2}-\d{2}),(?<aircraftId>.*?),(?<from>.*?),(?<to>.*?),(?<route>.*?),.*,(?<comments>.*)/gi;
+    } else if (logFormat == 'GarminPilot') {
+      re = /^(?<date>\d{1,2}\/\d{1,2}\/\d{4}),(?<aircraftId>.*?),(([^,]*),){29}(?<from>[^,]*),(?<to>[^,]*),(?<route>[^,]*)/gi;
+    }
+
     // Each line in input.txt will be successively available here as `line`.
-    const re = /^(?<date>\d{4}-\d{2}-\d{2}),(?<aircraftId>.*?),(?<from>.*?),(?<to>.*?),(?<route>.*?),.*,(?<comments>.*)/gi;
     const parsed = re.exec(line);
+
     if (parsed) {
-      // this is a valid logbook line, let's start iterating over the route
-      const aptFrom = await airportLookup(parsed.groups.from);
-      const aptTo = await airportLookup(parsed.groups.to);
+      // Skipping an entry if either the from or to fields are empty strings.
+      if (parsed.groups.from !== '' && parsed.groups.to !== '') {
 
-      if (parsed.groups.route) {
-        // we have a route, let's see if we can break it into parts
-        rt = parsed.groups.route.split(/,| |-/g)
-        rt = rt.filter(r => r);  // Remove any empty elements
+        // this is a valid logbook line, let's start iterating over the route
+        const aptFrom = await airportLookup(parsed.groups.from);
+        const aptTo = await airportLookup(parsed.groups.to);
 
-        let routeSteps = [];
-        for await(const el of rt) {
-          const aptStep = await airportLookup(el);
-          if (aptStep) { // Only add a routing step if we have an airport lookup success.
-            // TODO: Fix where airport ID and VOR are the same. Either that, or enforce that all logbook entries use 'K' where applicable. MyFlightLog has this same issue.
-            routeSteps.push(aptStep);
+        if (parsed.groups.route) {
+          // we have a route, let's see if we can break it into parts
+          rt = parsed.groups.route.split(/,| |-/g)
+          rt = rt.filter(r => r);  // Remove any empty elements
+
+          let routeSteps = [];
+          for await (const el of rt) {
+            const aptStep = await airportLookup(el);
+            if (aptStep) { // Only add a routing step if we have an airport lookup success.
+              // TODO: Fix where airport ID and VOR are the same. Either that, or enforce that all logbook entries use 'K' where applicable. MyFlightLog has this same issue.
+              routeSteps.push(aptStep);
+            }
           }
-        }
 
-        routeSteps.unshift(aptFrom)
-        routeSteps.push(aptTo)
+          routeSteps.unshift(aptFrom)
+          routeSteps.push(aptTo)
 
-        const routeCount = routeSteps.length;
-        for (let index = 1; index < routeCount; index++) {
-          o = {                               // Adding a route entry to the global flights array
-            from: routeSteps[index-1],
-            to: routeSteps[index],
+          const routeCount = routeSteps.length;
+          for (let index = 1; index < routeCount; index++) {
+            o = {                               // Adding a route entry to the global flights array
+              from: routeSteps[index - 1],
+              to: routeSteps[index],
+              log: parsed.groups
+            }
+            output.push(o);
+          }
+        } else {
+          o = {
+            from: aptFrom,
+            to: aptTo,
             log: parsed.groups
           }
           output.push(o);
         }
-      } else {
-        o = {
-          from: aptFrom,
-          to: aptTo,
-          log: parsed.groups
-        }
-        output.push(o);  
       }
     }
   }
   return output;
 };
 
-const airportLookup = async(airportId) => {
+const detectLogbookFormat = async () => {
+  const fileStream = fs.createReadStream(logDataFile, 'utf-8');
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  // Search Strings per Log Type
+  const reForeFlight = /^ForeFlight/gi
+  const reGarminPilot = /^Date,Aircraft ID,Aircraft Type/gi
+
+  for await (const line of rl) {
+    const ff = reForeFlight.exec(line)
+    const gp = reGarminPilot.exec(line)
+    if (gp) { return ('GarminPilot') }
+    if (ff) { return ('ForeFlight') }
+  }
+}
+
+const airportLookup = async (airportId) => {
   return airports.find((el) => el.IDENT === airportId || el.ICAO_ID === airportId);
 };
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# ForeFlight Log Mapper
+# Flight Log Mapper
 
-This small nodejs application takes a [ForeFlight](https://foreflight.com) logbook CSV export, and along with the FAA database of airports, performs the data processing necessary to translate it into a CSV for use on [kepler.gl](https://kepler.gl) for mapping. 
+This small nodejs application takes a [ForeFlight](https://foreflight.com) or [Garmin Pilot](https://buy.garmin.com/en-US/US/p/115856) logbook CSV export, and along with the FAA database of airports, performs the data processing necessary to translate it into a CSV for use on [kepler.gl](https://kepler.gl) for mapping. 
 
 The code presently only works for US-based airports, as I am using an FAA sourced airport lookup table.
 
@@ -10,12 +10,15 @@ The code presently only works for US-based airports, as I am using an FAA source
 
 * Node.js v15 or greater
 * [FAA Airport Database from FAA Open Data](https://opendata.arcgis.com/datasets/e747ab91a11045e8b3f8a3efd093d3b5_0.csv) - https://adds-faa.opendata.arcgis.com
-* ForeFlight Logbook CSV Export - you can get this from your personal portal at https://plan.foreflight.com.
+* One of:
+  * ForeFlight Logbook CSV Export - you can get this from your personal portal at https://plan.foreflight.com.
+  * Garmin Pilot Logbook CSV Export - you can get this from your FlyGarmin portal under the Setup | Export to Spreadsheet button at https://fly.garmin.com/fly-garmin/logbook/#/setup. 
 
 ## Runing the Script
 
 * (optional) Update the USA airport database at `data/airports.csv` directory with the latest version from the FAA.
 * Place your exported logbook as `data/logbook.csv` in the data directory.
+* Run `npm install` to install dependencies.
 * Execute `node main.js`.
 * Take the `out.csv` that was generated and import at [kepler.gl](https://kepler.gl). 
 * Adjust map styles and such as required.
@@ -25,11 +28,13 @@ The code presently only works for US-based airports, as I am using an FAA source
 ## Limitations (Today)
 
 * Only available for US Airports
-* The script right now depends on a particular format for the "Route" field which follows ForeFlight's default format (spaces between waypoints). It does a best-case lookup to see if there's an airport match for an identifier. If a VOR and ID match (i.e. CTY VOR, CTY Airport), the script can't tell if it's the VOR or you landed there. May update this later to require some delimiter like MyFlightBook.com does.
+* The script right now depends on a particular format for the "Route" field which I look for spaces or dashes between waypoints. It does a best-case lookup to see if there's an airport match for an identifier. If a VOR and ID match (i.e. CTY VOR, CTY Airport), the script can't tell if it's the VOR or you landed there. I may update this later to require some delimiter like MyFlightBook.com does.
 
-## Disclaimer
+## Disclaimers
 
 ForeFlight is a trademark of the Boeing company. There is no connection between this app and the ForeFlight application or company. I am just a big fan and user that depends on the great software they make to ensure the safety of myself and my passengers when we fly. Thank you to the whole ForeFlight team for making such a great application! Please visit them at **[foreflight.com](https://foreflight.com)**, buy their software, and get out and fly!
+
+Garmin Pilot is a trademark of Garmin and again, there is no connection between the app and that awesome team. A fellow pilot encouraged me to adapt the code to work for their logbooks as well, and I was happy to. EFB's make flying safer and easier. Go out and get the one that works best for you!
 
 ## License
 
@@ -37,4 +42,4 @@ Please see the [`LICENSE`](LICENSE) file in this repository for details.
 
 ## Contributing
 
-Contributions are welcome and encouraged. This was a fun side project to create. I am sure there are others out there who can do it better. Please submit pull requests and improve it for everyone!
+Contributions are welcome and encouraged. This was a fun project to create. I am sure there are others out there who can do it better. Please submit pull requests and improve it for everyone!
